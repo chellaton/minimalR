662dc002cbb603e228d895890fef45ae9bae8aff
Ramesh Chellaton <ramesh.chellaton@gmail.com>
2022-12-31 19:46:32 -0500

    composite data ready

--- ReadFiles.R
+++ ReadFiles.R
@@ -18,11 +18,22 @@ shared <- read_tsv('baxter.subsample.shared',
   pivot_longer(-group, names_to = 'otu', values_to = 'count') 
 
 # Read metadata file
-metadata <- read_tsv('baxter.metadata.tsv') %>%
-  rename_all(tolower)
+metadata <- read_tsv('baxter.metadata.tsv',
+                     col_types = cols(sample=col_character(),
+                                      Hx_Prev=col_logical(),
+                                      Hx_of_Polyps=col_logical(),
+                                      Smoke=col_logical(),
+                                      Diabetic=col_logical())) %>%
+  rename_all(tolower) %>%
+  rename(group=sample) %>%
+  mutate(srn = dx_bin == 'Adenoma' | dx_bin == 'Cancer',
+         lesion = dx_bin == 'Adenoma' | dx_bin == 'Cancer' | dx_bin == 'Adv Adenoma')
 
 res <- inner_join(shared, taxonomy_data, by='otu') %>%
   group_by(group, taxonomy) %>%
   summarize(count = sum(count)) %>%
   group_by(group) %>%
-  mutate(rel_abund = count/sum(count))
+  mutate(rel_abund = count/sum(count)) %>%
+  ungroup() %>%
+  select(-count) %>%
+  inner_join(., metadata, by = 'group')
